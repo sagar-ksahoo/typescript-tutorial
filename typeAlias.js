3cd30eadf65322679db012d849bf9a8b0f31e3ee
sagar-ksahoo <sagarsahoo1729@gmail.com>
2023-01-29 09:54:55 +0530

    type aliases

--- typeAlias.js
+++ typeAlias.js
@@ -1,4 +1,14 @@
 "use strict";
+// type User = {
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// };
 exports.__esModule = true;
-function createUSer(user) { }
-createUSer({ name: "", email: "", isActive: true });
+var myUser = {
+    id: "0123",
+    name: "sagar",
+    email: "sagar@gmail.com",
+    isActive: true
+};
+myUser.email = "1234";
--- typeAlias.ts
+++ typeAlias.ts
@@ -1,13 +1,45 @@
+// type User = {
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// };
+
+// function createUser(user: User): User {
+//   return { name: "", email: "", isActive: true };
+// }
+
+// createUser({ name: "", email: "", isActive: true });
+
 type User = {
+  readonly id: string;
   name: string;
   email: string;
   isActive: boolean;
+  creditCardDetails?: number; // optional type
 };
 
-function createUSer(user: User): User {
-  return { name: "", email: "", isActive: true };
-}
+let myUser: User = {
+  id: "0123",
+  name: "sagar",
+  email: "sagar@gmail.com",
+  isActive: true,
+};
 
-createUSer({ name: "", email: "", isActive: true });
+myUser.email = "1234";
+
+// combining types
+
+type cardNumber = {
+  cardnumber: number;
+};
+
+type cardDate = {
+  cardDate: string;
+};
+
+type cardDetails = cardNumber &
+  cardDate & {
+    cvv: number;
+  };
 
 export {};
