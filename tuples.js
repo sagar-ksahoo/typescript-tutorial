c563004e93072672c324aafae930a8cf686e9a8e
sagar-ksahoo <sagarsahoo1729@gmail.com>
2023-01-29 12:25:35 +0530

    tuples

---
+++ tuples.js
@@ -0,0 +1,6 @@
+"use strict";
+// const user: (string | number | boolean)[] = ["sks", 1, true, 2];
+exports.__esModule = true;
+var tUser;
+tUser = ["sks", 123, true];
+var rgb = [255, 205, 123];
---
+++ tuples.ts
@@ -0,0 +1,9 @@
+// const user: (string | number | boolean)[] = ["sks", 1, true, 2];
+
+let tUser: [string, number, boolean];
+
+tUser = ["sks", 123, true];
+
+let rgb: [number, number, number] = [255, 205, 123];
+
+export {};
